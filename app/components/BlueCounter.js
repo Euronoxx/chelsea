3c92cc9402360a6cdc041be7874823849967fcbc
Kallol Ghosal <kallol@euronoxxgroup.com>
2024-12-04 13:48:54 +0530

    Footer CSS updated

--- app/components/BlueCounter.js
+++ app/components/BlueCounter.js
@@ -5,15 +5,15 @@ const BlueCounter = () => {
     return (
         <Container className="py-5 bluebg text-center">
             <Row>
-                <Col>
+                <Col md={4} sm={12} xs={12}>
                     <h1>100+</h1>
                     <p>Hospitals Served </p>
                 </Col>
-                <Col>
+                <Col md={4} sm={12} xs={12}>
                     <h1>8+</h1>
                     <p>Years of Innovation </p>
                 </Col>
-                <Col>
+                <Col md={4} sm={12} xs={12}>
                     <h1>500+</h1>
                     <p>Successful Deliveries</p>
                 </Col>
