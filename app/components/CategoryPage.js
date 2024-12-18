122260e4bfcc276eedd1033ac216a5f166d49ca2
Kallol Ghosal <kallol@euronoxxgroup.com>
2024-12-18 12:30:02 +0530

    More data added

--- app/components/CategoryPage.js
+++ app/components/CategoryPage.js
@@ -8,7 +8,7 @@ const CategoryPage = ({paramUrl}) => {
     function genstr (str) {
         str = str.toLowerCase().split('-');
         for (let i = 0; i < str.length; i++) {
-        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+            str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
         }
         return str.join(' ');
     }
