3ceb92600a0acad267315b35c7352e45540c116b
Kallol Ghosal <kallol@euronoxxgroup.com>
2024-12-31 14:51:57 +0530

    Page CSS updated

--- app/products/the-chelsea/dental-instruments/page.js
+++ app/products/the-chelsea/dental-instruments/page.js
@@ -1,14 +1,23 @@
 "ues client"
 import Header from "@/app/components/Header";
 import Footer from "@/app/components/Footer";
-import { Container } from "react-bootstrap";
+import { Button, Col, Container, Image, Row } from "react-bootstrap";
 
 const ChelseaDental = () => {
     return (
         <>
         <Header />
-        <Container>
-            <h1>Chelsea Dental Instruments</h1>
+        <Container className="py-5">
+            <Row>
+                <Col md={6} sm={12} xs={12}>
+                    <Image src="/img/chelsea-dental.jpg" fluid alt="Koning breast imager" />
+                </Col>
+                <Col md={6} sm={12} xs={12}>
+                    <h1 className="txtblack">Chelsea Dental Instrumentsr</h1>
+                    <p className="body22">For the First time in Breast Imaging History, KBCT has Introduced Co-Registration in Multiple Planes using TRUE 3D Volumetric Imaging</p>
+                    <Button variant="primary" size="lg" className="mt-4">Download Brochure</Button>
+                </Col>
+            </Row>
         </Container>
         <Footer />
         </>
--- app/products/the-chelsea/surgical-instruments/page.js
+++ app/products/the-chelsea/surgical-instruments/page.js
@@ -1,14 +1,23 @@
 "use client"
 import Header from "@/app/components/Header";
 import Footer from "@/app/components/Footer";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col, Button, Image } from "react-bootstrap";
 
 const ChelseaSurgical = () => {
     return (
         <>
         <Header />
-        <Container>
-            <h1>Chelsea Surgical Instruments</h1>
+        <Container className="py-5">
+            <Row>
+                <Col md={6} sm={12} xs={12}>
+                    <Image src="/img/chelsea-surgical.jpg" fluid alt="Koning breast imager" />
+                </Col>
+                <Col md={6} sm={12} xs={12}>
+                    <h1 className="txtblack">Chelsea Surgical Instruments</h1>
+                    <p className="body22">For the First time in Breast Imaging History, KBCT has Introduced Co-Registration in Multiple Planes using TRUE 3D Volumetric Imaging</p>
+                    <Button variant="primary" size="lg" className="mt-4">Download Brochure</Button>
+                </Col>
+            </Row>
         </Container>
         <Footer />
         </>
