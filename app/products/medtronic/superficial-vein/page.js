7e75e624fffcf250c0fcba259de7368ca1ef7858
Kallol Ghosal <kallol@euronoxxgroup.com>
2025-01-06 18:32:15 +0530

    Product data added

---
+++ app/products/medtronic/superficial-vein/page.js
@@ -0,0 +1,21 @@
+"use client"
+import Header from "@/app/components/Header";
+import Footer from "@/app/components/Footer";
+import CategoryPage from "@/app/components/CategoryPage";
+import { usePathname } from 'next/navigation';
+
+const SuperficialVein = () => {
+
+    const url = usePathname();
+    const purls = url.split("/");
+    console.log(purls[3]);
+
+    return (
+        <>
+        <Header />
+        <CategoryPage paramUrl={purls[3]} />
+        <Footer />
+        </>
+    );
+}
+export default SuperficialVein;
